0ee9a0b876be1a3c0d40bfb2d29c750074da0403
HARIHARAN.R <115937095+theHARI962n@users.noreply.github.com>
2026-07-19 13:45:30 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,49 @@
+
+class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        // Min Heap
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>(
+                (a, b) -> Integer.compare(a.val, b.val)
+            );
+
+        // Step 1:
+        // Add the first node of every linked list
+        for (ListNode head : lists) {
+
+            if (head != null) {
+                pq.offer(head);
+            }
+        }
+
+        // Dummy node to simplify building the result
+        ListNode dummy = new ListNode(-1);
+
+        // Tail points to the last node of our result
+        ListNode tail = dummy;
+
+        // Step 2:
+        // Process all nodes
+        while (!pq.isEmpty()) {
+
+            // Get smallest node
+            ListNode current = pq.poll();
+
+            // Attach it to result
+            tail.next = current;
+
+            // Move tail forward
+            tail = tail.next;
+
+            // Add next node from the same list
+            if (current.next != null) {
+                pq.offer(current.next);
+            }
+        }
+
+        // Skip dummy node
+        return dummy.next;
+    }
+}
