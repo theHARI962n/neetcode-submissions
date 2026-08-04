a6bb07b15950e7d02eabf3d21b8d3120c5a55ea4
HARIHARAN.R <115937095+theHARI962n@users.noreply.github.com>
2026-08-04 23:22:25 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,89 @@
+class Node {
+
+    int key;
+    int value;
+    Node prev;
+    Node next;
+
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LRUCache {
+
+    private HashMap<Integer, Node> map;
+    private int capacity;
+
+    private Node head;
+    private Node tail;
+
+    public LRUCache(int capacity) {
+
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        node.prev = tail.prev;
+        node.next = tail;
+
+        tail.prev.next = node;
+        tail.prev = node;
+    }
+
+    public int get(int key) {
+
+        if (!map.containsKey(key))
+            return -1;
+
+        Node node = map.get(key);
+
+        remove(node);
+        insert(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (map.containsKey(key)) {
+
+            Node node = map.get(key);
+
+            node.value = value;
+
+            remove(node);
+            insert(node);
+
+        } else {
+
+            if (map.size() == capacity) {
+
+                Node lru = head.next;
+
+                remove(lru);
+
+                map.remove(lru.key);
+            }
+
+            Node node = new Node(key, value);
+
+            map.put(key, node);
+
+            insert(node);
+        }
+    }
+}
